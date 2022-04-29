fb955580f60aadf102af632f63befce404a21d29
Michael C <michael@mchang.name>
2022-04-29 01:15:32 -0400

    add link and remove console log

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # sb-logos
 collection of bad SponsorBlock Logos
 
-Rights for each logo are owned by the respective submitters
+Rights for each logo are owned by the respective submitters 
+
+[https://sb-logo.mchang.workers.dev/](See one)
--- src/random.js
+++ src/random.js
@@ -6,7 +6,6 @@ async function redirectResponse() {
   const choices = ["vuetube", "ecoli"]
   const choice = choices[Math.floor(Math.random() * choices.length)]
   const picture = "https://logo.sb.mchang.xyz/logos/" + choice + ".png"
-  console.log(picture)
   return new Response('', {
     status: 301,
     headers: {
